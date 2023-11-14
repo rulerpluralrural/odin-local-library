8fddc91cf75445fc0fd940be2aafcccdbe6f5b1c
Gerald Javier <33073866+rulerpluralrural@users.noreply.github.com>
2023-11-14 21:17:27 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const path = require("path")
+const port = process.env.port || 3000;
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const mongoDB = "mongodb://127.0.0.1:27017/libdb";
+
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/user", usersRouter);
+app.use("/catalog", catalogRouter);
+
+main().catch((err) => console.log(err));
+async function main() {
+	await mongoose.connect(mongoDB);
+}
+
+app.listen(port, (err) => {
+	if (err) console.log(err);
+	console.log(`Server running on port ${port}`);
+});
